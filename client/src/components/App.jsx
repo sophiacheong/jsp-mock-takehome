92921fb358ee3b8147333a16fb142b22559618c3
sophiacheong <sophiaacheong@gmail.com>
2021-09-29 17:53:16 -0700

    added search bar and input label

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [username, setUsername] = useState('');
+
+  const search = () => {
+    console.log('hi');
+  }
+
   return (
-    <div>Hi</div>
+    <div>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Type here..." name="username" />
+      <button onClick={search}>Search</button>
+    </div>
   )
 }
 
