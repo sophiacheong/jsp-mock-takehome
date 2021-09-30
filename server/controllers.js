38f02bf42b59c8aa30f60160b189c386b3e19f6c
sophiacheong <sophiaacheong@gmail.com>
2021-09-29 20:03:36 -0700

    added a get gists and a get gists i

--- server/controllers.js
+++ server/controllers.js
@@ -1,8 +1,19 @@
 const axios = require('axios');
 
 const controller = {
-  getRepos: (req, res) => {
+  getGists: (req, res) => {
+    let {username} = req.headers;
 
+    axios.get(`https://api.github.com/users/${username}/gists`)
+      .then(result => res.status(200).send(result.data))
+      .catch(err => res.status(404).send(err));
+  },
+  gist: (req, res) => {
+    let { id } = req.params;
+
+    axios.get(`https://api.github.com/gists/${id}`)
+      .then(result => res.status(200).send(result.data))
+      .catch(err => res.status(404).send(err));
   }
 };
 
--- server/routers.js
+++ server/routers.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const controller = require('./controllers');
 
 router
-  .route('/searchRepo')
-  .get(controller.getRepos);
+  .route('/gists')
+  .get(controller.getGists);
+
+router
+  .route('/gist/:id')
+  .get(controller.gist);
 
 module.exports = router;
